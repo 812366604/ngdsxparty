b7580b87a8e98ffc3c5bb09b7cd39ce742101909
812366604 <812366604@qq.com>
2019-03-22 14:58:19 +0800

    3月22日新增mybatis generator,json,全局异常处理,BeanValidator,JsonMapper,ApplicationContextHelper上下文工具

--- src/main/java/org/ngdsx/partybuilding/web/Test.java
+++ src/main/java/org/ngdsx/partybuilding/web/Test.java
@@ -1,19 +1,28 @@
 package org.ngdsx.partybuilding.web;
 
-import java.util.Date;
 
+import org.ngdsx.partybuilding.common.ApplicationContextHelper;
+import org.ngdsx.partybuilding.common.JsonData;
+import org.ngdsx.partybuilding.dao.UserRankTitleMapper;
+import org.ngdsx.partybuilding.entity.UserRankTitle;
+import org.ngdsx.partybuilding.util.JsonMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/test")
-
+@Slf4j
 public class Test {
 
-	@RequestMapping("/hello")
+	@RequestMapping("/hello.json")
 	@ResponseBody
-	public String hello() {
-		return "good afternoon" + new Date();
+	public JsonData hello() {
+		UserRankTitleMapper m = ApplicationContextHelper.popBean(UserRankTitleMapper.class);
+		UserRankTitle t = m.selectByPrimaryKey(1);
+		System.out.print(JsonMapper.obj2String(t));		
+		return JsonData.success("good afternoon");		
 	}
 }
